be8c20a5699867f7cf74b2a9d4546905fa72efcb
yoonwoo <lkkjasd@naver.com>
2019-04-11 01:43:28 +0000

    gitignore|requirements.txt

--- instagram/settings.py
+++ instagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#=66!u&vdjzqbekg8+l(z5nzexfvxl*ibnymtt^!b-d3)kvpwu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
